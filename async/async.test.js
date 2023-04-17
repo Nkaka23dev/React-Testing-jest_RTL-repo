79e5ef015453881b62bf6fc15ebfeb820d6ebe60
Nkaka23dev <nkakaeric96@gmail.com>
2023-04-17 18:59:07 +0200

    feat: done with arry

--- async/async.test.js
+++
@@ -1,8 +0,0 @@
-const getPosts = require('./async');
-
-describe("It should check if returned value is true", () => {
-    it("check get postData function", async () => {
-        const data = await getPosts(2);
-        expect(data.id).toBe(2);
-    })
-})
